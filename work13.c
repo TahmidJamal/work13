0295234ba3cc0b5bbfcca98b0476a3a5ebe8ddc6
TahmidJamal <tjamal@stuy.edu>
2018-11-12 16:42:42 -0500

    fadsf

--- work13.c
+++ work13.c
@@ -23,7 +23,7 @@ static void sighandler (int signo) {
 		printf("Note: %s\n", note );
 		
 		char message[52];
-		strcat(message, time);
+		strcpy(message, time);
 		strcat(message, note);
 		strcat(message, "\n\n");
 		printf("message: %s\n", message );
